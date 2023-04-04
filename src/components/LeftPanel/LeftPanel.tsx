4274e5bd371929c0a4e85c36ad7ac4072f189ce2
kasemsan00 <106854077+kasemsan00@users.noreply.github.com>
2023-04-04 19:11:20 +0700

    dev

--- src/components/LeftPanel/LeftPanel.tsx
+++ src/components/LeftPanel/LeftPanel.tsx
@@ -1,16 +1,36 @@
 import { IoMdSettings } from "react-icons/Io";
+import { useState } from "react";
+import { Listbox } from "@headlessui/react";
+
+const people = [
+  { id: 1, name: "Durward Reynolds", unavailable: false },
+  { id: 2, name: "Kenton Towne", unavailable: false },
+  { id: 3, name: "Therese Wunsch", unavailable: false },
+  { id: 4, name: "Benedict Kessler", unavailable: true },
+  { id: 5, name: "Katelyn Rohan", unavailable: false },
+];
 
 export default function LeftPanel() {
+  const [selectedPerson, setSelectedPerson] = useState(people[0]);
   const UserRegister = () => {};
   const UserUnregister = () => {};
 
   return (
     <div className="left-panel">
-      <div className="">
-        <select className="select-option">
-          <option>Profile1</option>
-          <option>Profile2</option>
-        </select>
+      <div className="view-local">
+        <div className="local-video"></div>
+      </div>
+      <div className="input-form">
+        <Listbox value={selectedPerson} onChange={setSelectedPerson}>
+          <Listbox.Button>{selectedPerson.name}</Listbox.Button>
+          <Listbox.Options>
+            {people.map((person) => (
+              <Listbox.Option key={person.id} value={person} disabled={person.unavailable}>
+                {person.name}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Listbox>
         <IoMdSettings className="setting-btn" />
       </div>
       <button className="btn-primary" onClick={UserRegister}>
