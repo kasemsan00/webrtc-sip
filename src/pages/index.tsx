37aaf39e044145b9a2fc79d0cd09b39655de1c79
kasemsan00 <kasemsan.cho@gmail.com>
2023-05-02 13:13:18 +0700

    dev

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>WebRTC Test</title>
+        <title>WebRTC</title>
         <meta name="description" content="WebRTC" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
