3432fa783f29f7b4a07b6ec6c44e39eb4635391c
kasemsan00 <kasemsan.cho@gmail.com>
2023-05-08 08:30:34 +0700

    dev

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function Sidebar({ children }: Props) {
   return (
-    <div className="flex flex-col justify-between top-0 min-w-72 w-72 h-full z-50 -d left-0 bg-slate-200 shadow-xl overflow-hidden p-2">
+    <div className="flex flex-col justify-between top-0 min-w-72 w-72 h-full z-50 -d left-0 bg-slate-200 shadow-md drop-shadow-md overflow-hidden p-2">
       {children}
     </div>
   );
