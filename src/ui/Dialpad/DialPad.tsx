164b331036fda54944ce3fbe0a6cb0e5f19ae38a
kasemsan00 <106854077+kasemsan00@users.noreply.github.com>
2023-05-11 16:24:13 +0700

    dev

--- src/ui/Dialpad/DialPad.tsx
+++ src/ui/Dialpad/DialPad.tsx
@@ -1,43 +1,98 @@
-import { MdCall } from "react-icons/md";
+import { MdCall, MdBackspace } from "react-icons/md";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
-const Call = () => {
+const Delete = ({ handleClick }: { handleClick: () => void }) => {
   return (
-    <div className="flex justify-center items-center bg-white rounded-full w-20 h-20 text-3xl cursor-pointer bg-green-500">
-      <MdCall className="text-white" />
-    </div>
+    <motion.button
+      onClick={handleClick}
+      className="flex justify-center items-center rounded-full w-20 h-20 text-3xl cursor-pointer "
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.1 }}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      exit={{ opacity: 0 }}
+    >
+      <MdBackspace className="text-gray-400" />
+    </motion.button>
   );
 };
 
-const Number = ({ number }: { number: string }) => {
-  return <div className="flex justify-center items-center bg-white rounded-full w-20 h-20 text-3xl cursor-pointer">{number}</div>;
+const Call = () => {
+  return (
+    <motion.button
+      className="flex justify-center items-center rounded-full w-20 h-20 text-3xl cursor-pointer bg-green-500"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+    >
+      <MdCall className="text-white" />
+    </motion.button>
+  );
+};
+
+const Number = ({ number, handleClick }: { number: string; handleClick: (arg0: string) => void }) => {
+  return (
+    <motion.button
+      onClick={() => handleClick(number)}
+      className="flex justify-center items-center rounded-full w-20 h-20 text-3xl cursor-pointer
+      bg-gray-200
+      active:bg-gray-300
+      "
+      whileTap={{ scale: 0.9 }}
+    >
+      {number}
+    </motion.button>
+  );
 };
 
 export default function DialPad() {
+  const [callNumber, setCallNumber] = useState<string>("");
+  const handleClickNumber = (number: string) => {
+    console.log(number);
+    if (callNumber.length >= 10) return;
+    setCallNumber((state) => state + number);
+  };
+  const handleDelete = () => {
+    if (callNumber.length === 0) return;
+    setCallNumber((state) => state.slice(0, -1));
+  };
+
   return (
-    <div className="fixed flex flex-col justify-center items-center top-0 z-[999] bg-gray-400 w-80 h-[1000px] gap-2">
-      <div>Hello 9999</div>
-      <div className="flex flex-row gap-2 justify-center items-center">
-        <Number number={"1"} />
-        <Number number={"2"} />
-        <Number number={"3"} />
+    <div className="fixed flex flex-1 flex-col justify-center items-center z-[999] bg-white w-[500px] h-screen gap-4 right-0">
+      <div className="mb-4">
+        <input
+          className="text-4xl text-center focus:outline-none"
+          type="text"
+          placeholder=""
+          value={callNumber}
+          onChange={() => {}}
+        />
       </div>
-      <div className="flex flex-row gap-2 justify-center items-center">
-        <Number number={"4"} />
-        <Number number={"5"} />
-        <Number number={"6"} />
+      <div className="flex flex-row gap-4 justify-center items-center">
+        <Number number={"1"} handleClick={handleClickNumber} />
+        <Number number={"2"} handleClick={handleClickNumber} />
+        <Number number={"3"} handleClick={handleClickNumber} />
       </div>
-      <div className="flex flex-row gap-2 justify-center items-center">
-        <Number number={"7"} />
-        <Number number={"8"} />
-        <Number number={"9"} />
+      <div className="flex flex-row gap-4 justify-center items-center">
+        <Number number={"4"} handleClick={handleClickNumber} />
+        <Number number={"5"} handleClick={handleClickNumber} />
+        <Number number={"6"} handleClick={handleClickNumber} />
       </div>
-      <div className="flex flex-row gap-2 justify-center items-center">
-        <Number number={"*"} />
-        <Number number={"0"} />
-        <Number number={"#"} />
+      <div className="flex flex-row gap-4 justify-center items-center">
+        <Number number={"7"} handleClick={handleClickNumber} />
+        <Number number={"8"} handleClick={handleClickNumber} />
+        <Number number={"9"} handleClick={handleClickNumber} />
       </div>
-      <div className="flex flex-row gap-2 justify-center items-center">
+      <div className="flex flex-row gap-4 justify-center items-center">
+        <Number number={"*"} handleClick={handleClickNumber} />
+        <Number number={"0"} handleClick={handleClickNumber} />
+        <Number number={"#"} handleClick={handleClickNumber} />
+      </div>
+      <div className="flex flex-row gap-4 justify-center items-center">
+        <div className="w-20"></div>
         <Call />
+        {callNumber !== "" ? <Delete handleClick={handleDelete} /> : <div className="w-20"></div>}
       </div>
     </div>
   );
