f43aaede1519930c9206179afccac481b08baf13
kasemsan00 <106854077+kasemsan00@users.noreply.github.com>
2023-05-20 13:40:56 +0700

    dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "WebRTC",
+  title: "Web SIP",
   description: "WebRTC For Sip",
 };
 
