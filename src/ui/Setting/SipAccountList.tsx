5cd13c9271a502a5cf6047c6f80987e4f02ff350
Kasemsan Chompuwised <106854077+kasemsan00@users.noreply.github.com>
2024-07-15 14:36:25 +0700

    dev

--- src/ui/Setting/SipAccountList.tsx
+++ src/ui/Setting/SipAccountList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { deleteExtension, getExtension } from "@/request/request";
 import { useStore } from "@/store/useStore";
 
